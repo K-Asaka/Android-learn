a95ed08b7e6299750b4c6d63eafe6c440bce78e6
Katsuhiro Asaka <aska@chiba-fjb.ac.jp>
2020-05-13 00:06:46 +0900

    別画面で計算中画面の実装

--- Calculator/app/src/main/java/com/example/android/sample/calculator/AnotherCalcActivity.java
+++ Calculator/app/src/main/java/com/example/android/sample/calculator/AnotherCalcActivity.java
@@ -2,12 +2,147 @@ package com.example.android.sample.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;import android.view.View.OnClickListener;
 
-public class AnotherCalcActivity extends AppCompatActivity {
+
+public class AnotherCalcActivity extends AppCompatActivity
+    implements TextWatcher, View.OnClickListener {
+
+    // 上の「計算ボタン」を押したときのリクエストコード
+    private static final int REQUEST_CODE_ANOTHER_CALC_1 = 1;
+    // 下の「計算ボタン」を押したときのリクエストコード
+    private static final int REQUEST_CODE_ANOTHER_CALC_2 = 2;
+    // 上のEditText
+    private EditText numberInput1;
+    // 下のEditText
+    private EditText numberInput2;
+    // 演算子選択用のSpinner
+    private Spinner operatorSelector;
+    // 計算結果表示用のTextView
+    private TextView calcResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another_calc);
+
+        // 「戻る」ボタン
+        findViewById(R.id.backButton).setOnClickListener(this);
+
+        // 上のEditText
+        numberInput1 = (EditText)findViewById(R.id.numberInput1);
+        // 上のEditTextの文字入力イベントを受け取る
+        numberInput1.addTextChangedListener(this);
+
+        // 下のEditText
+        numberInput2 = (EditText)findViewById(R.id.numberInput2);
+        // 下のEditTextの文字入力イベントを受け取る
+        numberInput2.addTextChangedListener(this);
+
+        // 演算子選択用のSpinner
+        operatorSelector = (Spinner)findViewById(R.id.operatorSelector);
+
+        // 計算結果表示用のTextView
+        calcResult = (TextView)findViewById(R.id.calcResult);
     }
+
+    // 2つのEditTextに入力がされているかをチェックする
+    private boolean checkEditTextInput() {
+        // 入力内容を取得する
+        String input1 = numberInput1.getText().toString();
+        String input2 = numberInput2.getText().toString();
+        // 2つとも空文字列(あるいはnull)でなければ、true
+        return !TextUtils.isEmpty(input1) && !TextUtils.isEmpty(input2);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        // テキストが変更される直前に呼ばれる。sは変更前の内容
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        // テキストが変更されるときに呼ばれる。sは変更後の内容で編集不可
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        // テキストが変更された後に呼ばれる。sは変更後の内容で編集可能
+        // 必要があれば計算を行い、結果を表示する
+        refreshResult();
+    }
+
+    // 計算結果の表示を更新する
+    private void refreshResult() {
+        if (checkEditTextInput()) {
+            // 計算を行う
+            int result = calc();
+
+            // 計算結果用のTextViewを書き換える
+            String resultText = getString(R.string.calc_result_text, result);
+            calcResult.setText(resultText);
+        } else {
+            // どちらかが入力されていない状態の場合、計算結果用の表示をデフォルトに戻す
+            calcResult.setText(R.string.calc_result_default);
+        }
+    }
+
+    // 計算を行う
+    private int calc() {
+        // 入力内容を取得する
+        String input1 = numberInput1.getText().toString();
+        String input2 = numberInput2.getText().toString();
+
+        // int型に変換する
+        int number1 = Integer.parseInt(input1);
+        int number2 = Integer.parseInt(input2);
+
+        // Spinnerから、選択中のindexを取得する
+        int operator = operatorSelector.getSelectedItemPosition();
+
+        // indexに応じて計算結果を返す
+        switch (operator) {
+            case 0: // 足し算
+                return number1 + number2;
+            case 1: // 引き算
+                return number1 - number2;
+            case 2: // 掛け算
+                return number1 * number2;
+            case 3: // 割り算
+                return number1 / number2;
+            default:
+                // 通常発生しない
+                throw new RuntimeException();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        // 「戻る」ボタンが押されたとき
+        // どちらかのEditTextに値が入っていない場合
+        if (!checkEditTextInput()) {
+            // キャンセルとみなす
+            setResult(RESULT_CANCELED);
+        } else {
+            // 計算結果
+            int result = calc();
+
+            // インテントを生成し、計算結果を詰める
+            Intent data = new Intent();
+            data.putExtra("result", result);
+            setResult(RESULT_OK, data);
+        }
+
+        // アクティビティを終了
+        finish();
+    }
+
 }
