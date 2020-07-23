c8eb632185c847e81e0dfe87bfe9588119a54a37
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2020-07-23 17:04:28 +0900

    Debugの表示を消し、Hello worldの表示位置を画面中央に変更

--- flutter_header_futter/lib/main.dart
+++ flutter_header_futter/lib/main.dart
@@ -8,8 +8,9 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Text("Hello world!!"),
+        body: Center(child: Text("Hello world!!")),
       ),
     );
   }
