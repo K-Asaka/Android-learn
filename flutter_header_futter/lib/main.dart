be2c45af8cb8113f73d4dce88e63094f8f43124b
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2020-07-23 17:26:03 +0900

    フッター部分を追加

--- flutter_header_futter/lib/main.dart
+++ flutter_header_futter/lib/main.dart
@@ -13,6 +13,18 @@ class App extends StatelessWidget {
       home: Scaffold(
         appBar: Header(),
         body: Center(child: Text("Hello world!!")),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home'),
+            ),
+          ],
+        ),
       ),
     );
   }
