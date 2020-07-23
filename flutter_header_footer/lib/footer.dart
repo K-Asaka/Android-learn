27140b5d61da12e979ab1f42249853d7e84a0797
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2020-07-23 19:40:37 +0900

    フッターの完成

--- flutter_header_footer/lib/footer.dart
+++ flutter_header_footer/lib/footer.dart
@@ -7,20 +7,81 @@ class Footer extends StatefulWidget {
   _Footer createState() => _Footer();
 }
 
-class _Footer extends State {
+class _Footer extends State<Footer> {
+  int _selectedIndex = 0;
+  final _bottomNavigationBarItems = <BottomNavigationBarItem>[];
+
+  // アイコン情報
+  static const _footerIcons = [
+    Icons.home,
+    Icons.textsms,
+    Icons.access_time,
+    Icons.content_paste,
+    Icons.work,
+  ];
+
+  static const _footerItemNames = [
+    'ホーム',
+    'トーク',
+    'タイムライン',
+    'ニュース',
+    'ウォレット',
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _bottomNavigationBarItems.add(_UpdateActiveState(0));
+    for (var i = 1; i < _footerItemNames.length; i++) {
+      _bottomNavigationBarItems.add(_UpdateDeactiveState(i));
+    }
+  }
+
+  BottomNavigationBarItem _UpdateActiveState(int index) {
+    return BottomNavigationBarItem(
+      icon: Icon(
+        _footerIcons[index],
+        color: Colors.black87,
+      ),
+      title: Text(
+        _footerItemNames[index],
+        style: TextStyle(
+          color: Colors.black87,
+        ),
+      )
+    );
+  }
+
+  BottomNavigationBarItem _UpdateDeactiveState(int index) {
+    return BottomNavigationBarItem(
+      icon: Icon(
+        _footerIcons[index],
+        color: Colors.black26,
+      ),
+      title: Text(
+        _footerItemNames[index],
+        style: TextStyle(
+          color: Colors.black26,
+        ),
+      )
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _bottomNavigationBarItems[_selectedIndex] = _UpdateDeactiveState(_selectedIndex);
+      _bottomNavigationBarItems[index] = _UpdateActiveState(index);
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          title: Text('Home'),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          title: Text('Home'),
-        ),
-      ],
+      type: BottomNavigationBarType.fixed,
+      items: _bottomNavigationBarItems,
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
     );
   }
 }
