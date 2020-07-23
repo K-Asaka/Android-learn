685cecac19743b4959216421077dc120170823b5
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2020-07-23 17:19:30 +0900

    ヘッダにアイコンを追加し背景色を変更

--- flutter_header_futter/lib/header.dart
+++ flutter_header_futter/lib/header.dart
@@ -7,7 +7,22 @@ class Header extends StatelessWidget with PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: Text('ホーム'),
+      leading: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Icon(Icons.settings),
+      ),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Icon(Icons.add),
+        ),
+      ],
+      title: Text(
+          'ホーム',
+      ),
+      backgroundColor: Colors.black87,
+      centerTitle: true,
+      elevation: 0.0,
     );
   }
 }
